e894528111f642a06e71921afe3e8c27cecaaef8
胡子豪 <wuqieor@outlook.com>
2020-09-16 02:12:19 +0800

    基本查询练习

--- myemployees/BasicQuery.sql
+++ myemployees/BasicQuery.sql
@@ -80,5 +80,59 @@ SELECT
 	CONCAT(last_name,first_name) 姓名 
 FROM employees;
 
-SELECT CONCAT(1,1,1);
-SELECT CONCAT(NULL,NULL,NULL)l;
+SELECT CONCAT(1,1,'hello');
+SELECT CONCAT(NULL,NULL,NULL);
+
+USE myemployees;
+-- 1. 下面的语句是否可以执行成功 select last_name , job_id , salary as sal from employees;
+SELECT
+	last_name,
+	job_id,
+	salary AS sal
+FROM
+	employees;
+	
+-- 2. 下面的语句是否可以执行成功 select * from employees;
+SELECT
+	*
+FROM
+	employees;
+	
+-- 3. 找出下面语句中的错误
+-- select employee_id , last_name，
+-- 	salary * 12 “ANNUAL SALARY”
+-- from employees;
+SELECT
+	employee_id,
+	last_name,
+	salary * 12 "ANNUAL SALARY"
+FROM
+	employees;
+	
+-- 4. 显示表 departments 的结构，并查询其中的全部数据
+DESC departments;
+
+SELECT
+ *
+FROM
+	departments;
+	
+SELECT
+	department_id 部门编号,
+	department_name 部门名,
+	manager_id 经理的员工编号,
+	location_id 位置编号
+FROM
+	departments;
+	
+-- 5. 显示出表 employees 中的全部 job_id(不能重复)
+SELECT
+	DISTINCT job_id
+FROM
+	employees;
+
+-- 6. 显示出表 employees 的last_name和first_name，各个列之间用逗号连接，列头显示成 OUT_PUT
+SELECT
+	CONCAT(IFNULL(last_name, 'no last name'), ',', IFNULL(first_name, 'no first name')) OUT_PUT
+FROM
+	employees;
